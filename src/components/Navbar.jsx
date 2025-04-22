47c521dd020c15d6eb9541e6d4bd0adbfab62649
Vishal Rathod <vrathod591@gmail.com>
2025-04-23 02:02:18 +0530

    Navbar component done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='h-20 flex w-full items-center text-white'>
-        <div className='text-3xl pl-20 font-semibold'>
-            Job Board
-        </div>
+    <div className="h-20 flex w-full items-center text-white">
+      <div className="text-3xl pl-20 font-semibold">Job Board</div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
