afb65479fb93f99446b596a00c5ad85eb693936c
Vishal Rathod <vrathod591@gmail.com>
2025-04-23 01:55:11 +0530

    Components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
 
   return (
-    <>
-    <p className="text-blue-700 text-2xl">My job <span className="text-blue-500 text-xl">Portal</span></p>
-    </>
+    <div>
+
+    </div>
   )
 }
 
