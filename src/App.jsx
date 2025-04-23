0cc7be43a8f73ff044dec3c49617cce947cbfa79
Vishal Rathod <vrathod591@gmail.com>
2025-04-23 23:55:12 +0530

    Header content adding

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Header />
-      <JobCard />
+      {/* <JobCard /> */}
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Header = () => {
+
   return (
-    <div>
-      <h1>One Stop Solution For Your Job Search</h1>
-      <h3>Get Latest Job Openings, Here...!</h3>
+    <div className="mt-10 flex flex-col gap-5 items-center justify-center text-white">
+      <h1 className="text-4xl ">One Stop Solution For Your Job Search</h1>
+      <h3 className="text-xl">Get Latest Job Openings, Here...!</h3>
     </div>
   );
 };
