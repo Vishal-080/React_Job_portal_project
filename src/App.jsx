6abff65c4d4cc2e85fc4cf951fa5f01bfd7a7547
Vishal Rathod <vrathod591@gmail.com>
2025-04-24 01:19:15 +0530

    Search content adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import JobCard from "./components/JobCard";
 import Navbar from "./components/Navbar";
+import SearchJob from "./components/SearchJob";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Header />
+      <SearchJob />
       {/* <JobCard /> */}
     </div>
   )
