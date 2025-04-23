2d03119f60ab7886fcd9b10af7e2bd90414106f9
Vishal Rathod <vrathod591@gmail.com>
2025-04-23 22:18:35 +0530

    Header content adding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div><h1>One Stop Solution For Your Job Search</h1>
+    <div>
+      <h1>One Stop Solution For Your Job Search</h1>
       <h3>Get Latest Job Openings, Here...!</h3>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
