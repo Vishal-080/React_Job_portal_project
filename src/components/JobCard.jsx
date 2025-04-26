ab30ea71b23feac5beac8bc09f2bbc8cd253cabd
Vishal Rathod <vrathod591@gmail.com>
2025-04-26 18:14:02 +0530

    job card

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -6,14 +6,14 @@ const JobCard = (props) => {
   let date1 = dayjs(Date.now());
   let diffDate = date1.diff("04-20-2025", "day");
   console.log(props.jobData.title, "data");
-  const {title, id, company } = props.jobData;
+  const {title, company,number_of_opening } = props.jobData;
   
   return (
     <div className='w-10/12 mx-auto my-2'>
       <div className='bg-zinc-200 px-6 py-4 rounded-xl shadow-2xl flex justify-between items-center hover:border hover:border-blue-500 hover:bg-zinc-100'>
         <div className='flex flex-col items-start gap-2'>
           <h1 className='font-semibold'>{title} - {props.company}</h1>
-          <p>Full-time &#x2022; 4 Openings</p>
+          <p>Full-time &#x2022; {number_of_opening} Openings</p>
           <div className='flex items-center gap-2'>
             {skills.map((item)=><p className='px-2 py-1 rounded-xl border'>{item}</p>)}
           </div>
