261bc63d3684819901a5e72a6ebd56d5eedc6482
Vishal Rathod <vrathod591@gmail.com>
2025-05-02 01:26:26 +0530

    Search job

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import JobCard from "./components/JobCard";
 import Navbar from "./components/Navbar";
 import SearchJob from "./components/SearchJob";
 import {data} from "./assets/dummyJobData.json";
+import { useState } from "react";
 
 function App() {
+  const [jobData,setJobData] = useState(null);
 
-  const filteredData = {
-      
+  const filteredJobData = (props)=>{
+      console.log("Hello", data.filter(item => (item.location==props.Location && item.location==props.Location )));
   };
   
   return (
     <div>
       <Navbar />
       <Header />
-      <SearchJob />
+      <SearchJob filteredJobData={filteredJobData}/>
       {data.map((item)=><JobCard key={item.id} jobData={item}/>)}
     </div>
   )
