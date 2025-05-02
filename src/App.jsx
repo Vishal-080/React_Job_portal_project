3ae6b65420601fc19e3140d1784da949756ff9fe
Vishal Rathod <vrathod591@gmail.com>
2025-05-02 22:17:17 +0530

    data filtered according to single search select

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,35 @@ import Header from "./components/Header";
 import JobCard from "./components/JobCard";
 import Navbar from "./components/Navbar";
 import SearchJob from "./components/SearchJob";
-import {data} from "./assets/dummyJobData.json";
+import { data } from "./assets/dummyJobData.json";
 import { useState } from "react";
 
 function App() {
-  const [jobData,setJobData] = useState(null);
+  const [jobData, setJobData] = useState(null);
 
-  const filteredJobData = (props)=>{
-    setJobData("Hello", data.filter(item => (item.employment_type==props.Job_type || item.location==props.Location || item.title==props.Job_title )));
-     // (jobData, "job data");
+  const filteredJobData = (props) => {
+    setJobData(
+      "Hello",
+      data.filter(
+        (item) =>
+          item.employment_type == props.Job_type ||
+          item.location == props.Location ||
+          item.title == props.Job_title
+      )
+    );
+    // (jobData, "job data");
   };
-  
+
   return (
     <div>
       <Navbar />
       <Header />
-      <SearchJob filteredJobData={filteredJobData}/>
-      {jobData ? jobData?.map((item)=><JobCard key={item.id} jobData={item}/>) :
-      data.map((item)=><JobCard key={item.id} jobData={item}/>)}
+      <SearchJob filteredJobData={filteredJobData} />
+      {jobData
+        ? jobData?.map((item) => <JobCard key={item.id} jobData={item} />)
+        : data.map((item) => <JobCard key={item.id} jobData={item} />)}
     </div>
-  )
+  );
 }
 
 export default App;
