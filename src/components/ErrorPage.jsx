892db3dc441e46856a295d753ad7c9e63aaafdb1
Vishal Rathod <vrathod591@gmail.com>
2025-05-06 22:48:33 +0530

    Error component coding

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const ErrorPage = () => {
   return (
     <div className="text-white text-3xl text-center my-6">
-          No Job Found...! <br /> Please Reload
-        </div>
-  )
-}
+      No Job Found...! <br /> Please Reload
+    </div>
+  );
+};
 
 export default ErrorPage;
