1185fc93b9ac2285f82a5de59fb7cfa37b452126
Vishal Rathod <vrathod591@gmail.com>
2025-05-08 23:52:43 +0530

    Main component coding

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { ErrorBoundary } from "react-error-boundary";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ErrorBoundary fallback={<p className='text-white my-20 text-center text-3xl font-bold'>Something Went Wrong ! <br/><br/><br/> please reload...</p>}>
+    <ErrorBoundary
+      fallback={
+        <p className="text-white my-20 text-center text-3xl font-bold">
+          Something Went Wrong ! <br />
+          <br />
+          <br /> please reload...
+        </p>
+      }
+    >
       <App />
     </ErrorBoundary>
-  </StrictMode>,
-)
+  </StrictMode>
+);
